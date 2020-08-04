1af662a34aa2af353574f90ee1c0a9092edac23b
Colvan <yoniborisenco@gmail.com>
2020-08-04 20:58:04 +0300

    Updates

---
+++ precache-manifest.825704e58fc55a3e563d0b6b7a4d6dd9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf389ef66788f0f9f6558c2daa90ef19",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "e6d4a9b7e5da02d7d989",
+    "url": "/my-website/static/css/main.2dc15ffd.chunk.css"
+  },
+  {
+    "revision": "44c80ed5220a987d4d8a",
+    "url": "/my-website/static/js/2.71d56e50.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-website/static/js/2.71d56e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d4a9b7e5da02d7d989",
+    "url": "/my-website/static/js/main.19c998d1.chunk.js"
+  },
+  {
+    "revision": "8a31e1a8153659a682c8",
+    "url": "/my-website/static/js/runtime-main.0cbcd75a.js"
+  },
+  {
+    "revision": "80d0d648001d192a42d990c8efc48308",
+    "url": "/my-website/static/media/IvanBorisencoResume.80d0d648.pdf"
+  },
+  {
+    "revision": "e48be8632849e9a4f5306f67cb1fd75c",
+    "url": "/my-website/static/media/IvanBorisencoResume.e48be863.jpg"
+  },
+  {
+    "revision": "8743012af94b44077b02135f18d7ec1e",
+    "url": "/my-website/static/media/alien.8743012a.svg"
+  },
+  {
+    "revision": "457d5118693ca47ea124c40df14e5bc7",
+    "url": "/my-website/static/media/me.457d5118.png"
+  }
+]);
