4a55d2184763600a0bdc18ae1ae992c10c1e7b51
colvan <yoniborisenco@gmail.com>
2020-08-21 16:34:46 +0300

    added new gh pages

--- src/pages/resumepage/resumepage.component.jsx
+++ src/pages/resumepage/resumepage.component.jsx
@@ -6,6 +6,7 @@ import MyPDF from "../../assets/IvanBorisencoResume.pdf";
 function ResumePage() {
   return (
     <div>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <img alt="mySelf" src={photo} className="resume" />
       <a href={MyPDF} download>
         <button className="download">DOWNLOAD RESUME</button>
